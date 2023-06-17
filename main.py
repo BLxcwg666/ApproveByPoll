c515ebcebe3b10d8aae3b7600285b852fb37e311
BLxcwg666 <huixcwg@gmail.com>
2023-06-17 15:41:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-	await message.reply("我是你爹")
+	await message.reply("~\(≧▽≦)/~")
 
 
 @dp.chat_join_request_handler()
@@ -34,24 +34,24 @@ async def join(request: types.ChatJoinRequest):
 	polling = await bot.stop_poll(chat_id, polling.message_id)
 
 	if polling.total_voter_count == 0:
-		await message.reply("No one voted.")
-		await bot.send_message(user_id, "No one voted. Please request again later.")
-		logger.info(f"No one voted for {username}({user_id}) in chat {chat_id}.")
+		await message.reply("没有人投票哦~")
+		await bot.send_message(user_id, "暂时没有人投票哦，请等一会再来申请吧~")
+		logger.info(f"没人给 {username}({user_id}) 在 {chat_id} 投票")
 		await request.decline()
 	elif polling.options[0].voter_count > polling.options[1].voter_count:
-		await message.reply("Approved.")
-		await bot.send_message(user_id, "You have been approved.")
-		logger.info(f"{username}({user_id}) has been approved in chat {chat_id}.")
+		await message.reply("通过~")
+		await bot.send_message(user_id, "已经通过申请了喵~")
+		logger.info(f"{username}({user_id}) 已经在群组 {chat_id} 被批准加入")
 		await request.approve()
 	elif polling.options[0].voter_count == polling.options[1].voter_count:
-		await message.reply("No result.")
-		await bot.send_message(user_id, "No result. Please request again later.")
-		logger.info(f"No result for {username}({user_id}) in chat {chat_id}.")
+		await message.reply("没有结果~")
+		await bot.send_message(user_id, "投票没有分出胜负哦，请等一会再来申请吧~")
+		logger.info(f"对于 {username}({user_id}) 在 {chat_id} 的投票没有结果")
 		await request.decline()
 	else:
-		await message.reply("Denied.")
-		await bot.send_message(user_id, "You have been denied.")
-		logger.info(f"{username}({user_id}) has been denied in chat {chat_id}.")
+		await message.reply("拒绝！")
+		await bot.send_message(user_id, "你被拒绝加入啦！")
+		logger.info(f"{username}({user_id}) 已经在群组 {chat_id} 被拒绝加入")
 		await request.decline()
 
 
